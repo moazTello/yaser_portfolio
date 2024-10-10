3c26ddb9830b1668c26deb676106fc4744402f55
moazTello <tello.m.m.t.95@gmail.com>
2024-10-10 03:05:51 +0300

    fix comma

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -78,7 +78,9 @@ const EmailSection = () => {
         transition={{ duration: 3 }}
         className="z-10"
       >
-        <h5 className="text-xl font-bold text-white my-2">Lest's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Lest&apos;s Connect
+        </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
