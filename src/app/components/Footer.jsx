8f450ce8730947dc2cf97e13034ce40b3b98e28e
moazTello <tello.m.m.t.95@gmail.com>
2024-10-13 20:54:45 +0300

    test_color

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className="footer grid border z-10 py-8 px-10 border-t-[#33353F] border-l-transparent border-b-transparent border-r-transparent text-white">
       <div className="container grid md:grid-cols-2">
-        <p>LOGO</p>
         <div className="flex flex-col items-center justify-center">
           <p className="text-slate-200 text-center">All rights reserved </p>
           <p className="text-md text-primary-500 text-center">
