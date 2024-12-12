505dd7070eb9f601e92cef3feb4b97b4c26c90a7
moazTello <tello.m.m.t.95@gmail.com>
2024-12-12 17:50:23 +0300

    after_freedom_1

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,141 +1,122 @@
-"use client";
-import React, { useState, useRef } from "react";
-import ProjectCard from "./ProjectCard";
-import ProjcetTag from "./ProjcetTag";
-import { motion, useInView } from "framer-motion";
+'use client';
+import React, { useState, useRef } from 'react';
+import ProjectCard from './ProjectCard';
+// import ProjcetTag from "./ProjcetTag";
+import { motion, useInView } from 'framer-motion';
 
 const ProjectsSection = () => {
   const ProjectData = [
     {
       id: 1,
-      title: "WAYGO",
+      title: 'Scheduler',
       description: (
         <div className="flex w-full md:pl-4 flex-col">
-          <p className="mt-4 text-primary-500">Bus Booking Web App</p>
+          <p className="mt-4 text-primary-500">Schedualing Projects App</p>
           <ul className="list-disc px-2 mt-2 ml-2">
             <li>
-              A web application designed to streamline the booking process for
-              bus trips across the Syrian Arab Republic.
-            </li>
-            <li>
-              The platform offers bilingual support Arabic and English and
-              integrates electronic payment systems to provide users with a
-              seamless and efficient booking experience.
-            </li>
-            <li>
-              Users can easily search for available bus routes, select their
-              preferred travel times, and secure their tickets online.
-            </li>
-          </ul>
-          <p className="mt-4 text-slate-100">Key Features:</p>
-          <ul className="list-disc px-2 mt-2 ml-2">
-            <li>
-              Bilingual Support: Full support for Arabic and English languages
-              to accommodate a diverse user base.
-            </li>
-            <li>
-              Secure Payments: Integrated electronic payment gateway for quick
-              and secure transactions.
-            </li>
-            <li>
-              Real-Time Availability: Users can view real-time bus schedules and
-              seat availability.
-            </li>
-            <li>
-              Responsive Design: Optimized for various screen sizes and devices
-              for easy access from anywhere.
-            </li>
-            <li>
-              Tech Stack: Built with React.js, ensuring fast rendering, smooth
-              performance.
-            </li>
-          </ul>
-          <p className="mt-4 text-slate-100">Tech Stack:</p>
-          <ul className="list-disc px-2 mt-2 ml-2">
-            <li>React.js: Provides a fast, dynamic user interface.</li>
-            <li>
-              useContext: Ensures efficient state management across components.
-            </li>
-            <li>
-              Zustand: Lightweight state management for better scalability and
-              performance.
-            </li>
-            <li>
-              Firebase: Used for backend services, including data storage and
-              authentication.
-            </li>
-            <li>Axios: Handles API requests efficiently.</li>
-            <li>
-              i18-next: Enables full multilingual support for an international
-              user base.
-            </li>
-            <li>
-              useRef: Facilitates direct DOM manipulation and better handling of
-              form inputs and other interactive elements.
-            </li>
-            <li>
-              react-icon: Adds icons for a more intuitive user experience.
-            </li>
-            <li>
-              react-router-dom: Manages seamless navigation between pages.
-            </li>
-            <li>
-              useEffect, useState, useMemo: Manage application lifecycle and
-              performance optimizations.
-            </li>
-          </ul>
-          <p className="mt-4 text-slate-100">Challenges:</p>
-          <ul className="list-disc px-2 mt-2 ml-2">
-            <li>
-              Ensuring a seamless experience across different languages and RTL
-              right-to-left support for Arabic.
-            </li>
-            <li>
-              Implementing secure and reliable electronic payment integration.
+              A website that helps in scheduling personal projects, tasks and organizing them, where each project has a
+              number of tasks that must be carried out and based on which the project status changes
             </li>
           </ul>
         </div>
       ),
-      image: "/images/projects/way_go_group.png",
-      tag: ["All", "Web"],
-      gitUrl: "https://github.com/moazTello/waygo_page",
-      previewUrl: "https://moaztello.github.io/waygo_page",
+      image: '/images/projects/schedular.png',
+      tag: ['All', 'Web'],
+      gitUrl: 'https://github.com/YaserAlsamsamSyr/projects',
+      previewUrl: 'https://myprojects.yaseralsamsam.com/',
+    },
+    {
+      id: 2,
+      title: 'NFC Cards',
+      description: (
+        <div className="flex w-full md:pl-4 flex-col">
+          <p className="mt-4 text-primary-500">NFC Cards App</p>
+          <ul className="list-disc px-2 mt-2 ml-2">
+            <li>
+              A web application that helps to inquire about employees and also allows the entry of a new employee,
+              modification of employee information, and deletion of an employee. The application also contains a page
+              for each employee to inquire about it through the employee's password, as it is considered as a username{' '}
+            </li>
+          </ul>
+        </div>
+      ),
+      image: '/images/projects/NFC.png',
+      tag: ['All', 'Web'],
+      gitUrl: 'https://github.com/YaserAlsamsamSyr/NFCcard',
+      previewUrl: 'https://nfccard.yaseralsamsam.com/',
+    },
+    {
+      id: 3,
+      title: 'Way GO',
+      description: (
+        <div className="flex w-full md:pl-4 flex-col">
+          <p className="mt-4 text-primary-500">Way GO App</p>
+          <ul className="list-disc px-2 mt-2 ml-2">
+            <li>
+              A web application directed to travel companies between governorates, as it helps a travel company to enter
+              new flights and book travelers on flights and also facilitates the process of inquiring about travelers on
+              any trip chosen by the travel company, whether the trip is active or finished, and the application also
+              helps to cancel the reservation of a traveler from a trip
+            </li>
+          </ul>
+        </div>
+      ),
+      image: '/images/projects/way_go_group.png',
+      tag: ['All', 'Web'],
+      gitUrl: 'https://github.com/YaserAlsamsamSyr/waygoDesktop',
+      previewUrl: 'https://waygodesktop.yaseralsamsam.com/',
+    },
+    {
+      id: 4,
+      title: 'Awtart',
+      description: (
+        <div className="flex w-full md:pl-4 flex-col">
+          <p className="mt-4 text-primary-500">Awtar App</p>
+          <ul className="list-disc px-2 mt-2 ml-2">
+            <li>
+              An online store for the sale of perfumes of Awtar Al-Sultanate Company and to inquire about its products,
+              and the store also contains a control panel to enter, modify and delete products. The store also has the
+              feature of sending WhatsApp messages to a control panel containing requests sent by customers
+            </li>
+          </ul>
+        </div>
+      ),
+      image: '/images/projects/Awtar.png',
+      tag: ['All', 'Web'],
+      gitUrl: 'https://github.com/YaserAlsamsamSyr/awtarAlsultanate',
+      previewUrl: 'http://awtaralsultanatee.yaseralsamsam.com/',
     },
   ];
   const projectsRef = useRef(null);
   const isInview = useInView(projectsRef, { once: true });
-  const [tag, setTag] = useState("All");
+  const [tag, setTag] = useState('All');
   const handleTagChange = (newTag) => {
     setTag(newTag);
   };
-  const filteredProjects = ProjectData.filter((project) =>
-    project.tag.includes(tag)
-  );
+  const filteredProjects = ProjectData.filter((project) => project.tag.includes(tag));
   const variants = {
     initial: { scale: 0.5, y: 50, opacity: 0 },
     animate: { scale: 1, y: 0, opacity: 1 },
   };
   return (
     <section>
-      <h2 className="text-center text-4xl font-bold text-white mt-4 ">
-        My Projects
-      </h2>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 ">My Projects</h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjcetTag
+        {/* <ProjcetTag
           name="All"
           onClick={handleTagChange}
           isSelected={tag === "All"}
-        />
-        <ProjcetTag
+        /> */}
+        {/* <ProjcetTag
           name="Web"
           onClick={handleTagChange}
           isSelected={tag === "Web"}
-        />
-        <ProjcetTag
+        /> */}
+        {/* <ProjcetTag
           name="Mobile"
           onClick={handleTagChange}
           isSelected={tag === "Mobile"}
-        />
+        /> */}
         {/* <ProjcetTag
           name="Design"
           onClick={handleTagChange}
@@ -149,7 +130,7 @@ const ProjectsSection = () => {
             key={index}
             variants={variants}
             initial="initial"
-            animate={isInview ? "animate" : "initial"}
+            animate={isInview ? 'animate' : 'initial'}
             transition={{ duration: 1, delay: index * 0.8 }}
           >
             <ProjectCard
